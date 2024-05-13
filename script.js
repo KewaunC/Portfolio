b3395a9097bf481af90a39dc768bbcc57946b20c
Kewaun Cain <kewaunscain@gmail.com>
2024-05-13 10:23:46 -0400

    removed nmap

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() {
     
-    document.addEventListener('selectstart', function(e) {
-        if (!e.target.classList.contains('email')) {
-            e.preventDefault();
-        }
-    });
+//     document.addEventListener('selectstart', function(e) {
+//         if (!e.target.classList.contains('email')) {
+//             e.preventDefault();
+//         }
+//     });
     
 
-    document.addEventListener('contextmenu', function(e) {
-        if (!e.target.classList.contains('email')) {
-            e.preventDefault();
-        }
-    });
+//     document.addEventListener('contextmenu', function(e) {
+//         if (!e.target.classList.contains('email')) {
+//             e.preventDefault();
+//         }
+//     });
     
     
-    const menuToggle = document.querySelector('.menu-toggle');
-    const menu = document.querySelector('.menu');
+//     const menuToggle = document.querySelector('.menu-toggle');
+//     const menu = document.querySelector('.menu');
 
-    menuToggle.addEventListener('click', () => {
-        menu.classList.toggle('active');
-    });
-});
+//     menuToggle.addEventListener('click', () => {
+//         menu.classList.toggle('active');
+//     });
+// });
